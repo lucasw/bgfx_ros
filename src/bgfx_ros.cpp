b0007b8c9d084caa3bd3165fc49655cce6bf3942
Lucas Walter <wsacul@gmail.com>
2016-12-30 06:50:25 -0800

    Builds but does nothing

---
+++ src/bgfx_ros.cpp
@@ -0,0 +1,14 @@
+/**
+  Copyright 2016 Lucas Walter
+
+  https://github.com/lucasw/bgfx_ros
+*/
+
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "bgfx_ros");
+
+  return 0;
+}
