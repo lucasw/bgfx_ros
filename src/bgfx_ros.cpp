7ffc79619c4046c7ea4ff32bed3da7fb73592160
Lucas Walter <wsacul@gmail.com>
2016-12-31 10:04:36 -0800

    interim checkin, doesn't compile

--- src/bgfx_ros.cpp
+++ src/bgfx_ros.cpp
@@ -5,10 +5,32 @@
 */
 
 #include <ros/ros.h>
+#include <bgfx/bgfx.h>
+
+class BgfxRos
+{
+  public:
+
+  uint32_t width_;
+  uint32_t height_;
+  uint32_t reset_;
+
+  void init()
+  {
+    bgfx::init();
+    bgfx::reset(width_, height_, reset_);
+  }
+
+}
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "bgfx_ros");
+
+  while (ros::ok())
+  {
+
+  }
 
   return 0;
 }
