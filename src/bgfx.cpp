02fc6b776de5e788519861ec8c89afebc19daa76
Lucas Walter <wsacul@gmail.com>
2017-02-04 07:07:38 -0800

    Compiles but crashes because something is wrong with the vertex decl:
    vertexdecl 08ef9b38 (f77a7737), stride 16
    	attr 0 - Attrib::Position, num 3, type 4, norm 0, asint 0, offset 0
    	attr 4 - Attrib::Color0, num 4, type 0, norm 1, asint 0, offset 12

--- src/bgfx.cpp
+++ src/bgfx.cpp
@@ -6,23 +6,48 @@
   ~/other/bgfx/.build/linux64_gcc/bin/shadercDebug -f fs_cubes.sc -i $HOME/other/bgfx/src -o fs_cubes.bin --varyingdef ./varying.def.sc --platform linux --type fragment
   ~/other/bgfx/.build/linux64_gcc/bin/shadercDebug -f vs_cubes.sc -i $HOME/other/bgfx/src -o vs_cubes.bin --varyingdef ./varying.def.sc --platform linux --type vertex
 
-  g++ bgfx.cpp -g -I$HOME/other/bgfx/include -I$HOME/other/bx/include -L$HOME/other/bgfx/.build/linux64_gcc/bin -lbgfx-shared-libDebug -lGLU -lGL `sdl2-config --cflags --libs`
+  g++ bgfx.cpp -g -I$HOME/other/bgfx/include -I$HOME/other/bx/include -L$HOME/other/bgfx/.build/linux64_gcc/bin -lbgfx-shared-libDebug -lGLU -lGL `sdl2-config --cflags --libs` -std=c++11
 */
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
 #include <bgfx/platform.h>
 #include <bgfx/bgfx.h>
+#include <bx/fpumath.h>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
 #include <vector>
 
+struct PosColorVertex
+{
+  float x_;
+  float y_;
+  float z_;
+  uint32_t abgr_;
+
+  static void init()
+  {
+    decl_
+      .begin()
+      .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+      .add(bgfx::Attrib::Color0,   4, bgfx::AttribType::Uint8, true)
+      .end();
+  };
+
+  static bgfx::VertexDecl decl_;
+};
+
+bgfx::VertexDecl PosColorVertex::decl_;
+
 int main(int argc, char** argv)
 {
+	uint32_t width = 1280;
+	uint32_t height = 720;
+
   SDL_Window* window = SDL_CreateWindow("bgfx_ros", SDL_WINDOWPOS_UNDEFINED,
-      SDL_WINDOWPOS_UNDEFINED, 1280, 720,
+      SDL_WINDOWPOS_UNDEFINED, width, height,
       SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
 	SDL_SysWMinfo wmi;
@@ -49,13 +74,13 @@ int main(int argc, char** argv)
     return -1;
   }
 
-  bgfx::reset(1280, 720, BGFX_RESET_VSYNC);
+  bgfx::reset(width, height, BGFX_RESET_VSYNC);
 
   bgfx::setDebug(BGFX_DEBUG_TEXT);
   bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH,
       0x303030ff, 1.0f, 0);
 
-
+  PosColorVertex::init();
   bgfx::ShaderHandle fhandle;
   {
     const std::string path = "fs_cubes.bin";
@@ -86,11 +111,65 @@ int main(int argc, char** argv)
   // TODO(lucasw) check if this worked
   program = bgfx::createProgram(vhandle, fhandle, true);
 
+  // Make geometry
+  std::vector<PosColorVertex> vertices;
+  for (size_t xi = 0; xi < 2; ++xi)
+  {
+    for (size_t yi = 0; yi < 2; ++yi)
+    {
+      for (size_t zi = 0; zi < 2; ++zi)
+      {
+        PosColorVertex pcv;
+        pcv.x_ = xi * 2.0 - 1.0;
+        pcv.y_ = yi * 2.0 - 1.0;
+        pcv.z_ = zi * 2.0 - 1.0;
+        pcv.abgr_ = 0xff000000 + xi * 0xff0000 + yi * 0xff00 + zi * 0xff;
+        vertices.push_back(pcv);
+      }
+    }
+  }
+
+  std::vector<uint16_t> triangle_list;
+  triangle_list.push_back(0);
+  triangle_list.push_back(1);
+  triangle_list.push_back(2);
+
+  triangle_list.push_back(1);
+  triangle_list.push_back(3);
+  triangle_list.push_back(2);
+
+  triangle_list.push_back(4);
+  triangle_list.push_back(6);
+  triangle_list.push_back(5);
+
+  triangle_list.push_back(0);
+  triangle_list.push_back(2);
+  triangle_list.push_back(4);
+
+  std::cout << "########## vertex ##########" << std::endl;
+  bgfx::VertexBufferHandle vbh;
+  {
+    bgfx::Memory mem;
+    mem.data = (uint8_t*)(&vertices[0]);
+    mem.size = vertices.size();
+    vbh = bgfx::createVertexBuffer(&mem, PosColorVertex::decl_);
+  }
+
+  bgfx::IndexBufferHandle ibh;
+  {
+    bgfx::Memory mem;
+    mem.data = (uint8_t*)&triangle_list[0];
+    mem.size = triangle_list.size();
+    ibh = bgfx::createIndexBuffer(&mem);
+  }
+
+	float at[3]  = { 0.0f, 0.0f,   0.0f };
+	float eye[3] = { 0.0f, 0.0f, -35.0f };
+
+  std::cout << "#########################" << std::endl;
   // while (true)
   for (size_t i = 0; i < 5; ++i)
   {
-    bgfx::touch(0);
-
     bgfx::dbgTextClear();
     std::stringstream ss;
     ss << "test " << i;
@@ -98,11 +177,54 @@ int main(int argc, char** argv)
         0x8f, ss.str().c_str());
     std::cout << ss.str() << std::endl;
 
+		float view[16];
+		bx::mtxLookAt(view, eye, at);
+
+		float proj[16];
+		bx::mtxProj(proj, 60.0f, float(width) / float(height), 0.1f, 100.0f);
+		bgfx::setViewTransform(0, view, proj);
+
+		// Set view 0 default viewport.
+		bgfx::setViewRect(0, 0, 0, uint16_t(width), uint16_t(height) );
+
+		bgfx::touch(0);
+
+		// draw a single cube
+		{
+      const float fr = i * 0.01;
+			uint32_t xx = 0.0;
+      uint32_t yy = 0.0;
+			float mtx[16];
+			bx::mtxRotateXY(mtx, fr + xx*0.21f, fr + yy*0.37f);
+			mtx[12] = -15.0f + float(xx)*3.0f;
+			mtx[13] = -15.0f + float(yy)*3.0f;
+			mtx[14] = 0.0f;
+
+			// Set model matrix for rendering.
+			bgfx::setTransform(mtx);
+
+			// Set vertex and index buffer.
+			bgfx::setVertexBuffer(vbh);
+			bgfx::setIndexBuffer(ibh);
+
+			// Set render states.
+			bgfx::setState(0
+				| BGFX_STATE_DEFAULT
+				// | BGFX_STATE_PT_TRILIST    // this doesn't exist
+				// | BGFX_STATE_PT_TRISTRIP    // this doesn't exist
+				);
+
+			// Submit primitive for rendering to view 0.
+			bgfx::submit(0, program);
+		}
+
     bgfx::frame();
 	  // pause();
     usleep(50000);
   }
 
+  bgfx::destroyIndexBuffer(ibh);
+  bgfx::destroyVertexBuffer(vbh);
   bgfx::shutdown();
 
   return 0;
